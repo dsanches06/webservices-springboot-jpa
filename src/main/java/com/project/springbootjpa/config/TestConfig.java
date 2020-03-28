2e93f8524c0a3a8fc8c9f0a31f59e8a0d47baa54
Danilson Sanches <dsanches06@outlook.com>
2020-03-28 08:56:02 +0000

    Payment, one-to-one association

--- src/main/java/com/project/springbootjpa/config/TestConfig.java
+++ src/main/java/com/project/springbootjpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.project.springbootjpa.entities.Category;
 import com.project.springbootjpa.entities.Order;
 import com.project.springbootjpa.entities.OrderItem;
+import com.project.springbootjpa.entities.Payment;
 import com.project.springbootjpa.entities.Product;
 import com.project.springbootjpa.entities.User;
 import com.project.springbootjpa.entities.enums.OrderStatus;
@@ -90,5 +91,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		// salvar na tabela order item
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		// payment - one to one
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		// salvar novamente na tabela order
+		orderRepository.save(o1);
 	}
 }
--- src/main/java/com/project/springbootjpa/entities/Payment.java
+++ src/main/java/com/project/springbootjpa/entities/Payment.java
@@ -2,26 +2,20 @@ package com.project.springbootjpa.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.project.springbootjpa.entities.enums.OrderStatus;
 
 @Entity
-@Table(name = "tb_order")
-public class Order implements Serializable {
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -32,26 +26,17 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 
-	private Integer orderStatus;
+	@OneToOne
+	@MapsId
+	private Order order;
 
-	@ManyToOne
-	@JoinColumn(name = " client_id")
-	private User client;
-
-	@OneToMany(mappedBy = "id.order")
-	private Set<OrderItem> items = new HashSet<>();
-
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment;
-
-	public Order() {
+	public Payment() {
 	}
 
-	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+	public Payment(Long id, Instant moment, Order order) {
 		this.id = id;
 		this.moment = moment;
-		setOrderStatus(orderStatus);
-		this.client = client;
+		this.order = order;
 	}
 
 	public Long getId() {
@@ -70,34 +55,12 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
-	public OrderStatus getOrderStatus() {
-		return OrderStatus.valueOf(orderStatus);
+	public Order getOrder() {
+		return order;
 	}
 
-	public void setOrderStatus(OrderStatus orderStatus) {
-		if (orderStatus != null) {
-			this.orderStatus = orderStatus.getCode();
-		}
-	}
-
-	public User getClient() {
-		return client;
-	}
-
-	public void setClient(User client) {
-		this.client = client;
-	}
-
-	public Payment getPayment() {
-		return payment;
-	}
-
-	public void setPayment(Payment payment) {
-		this.payment = payment;
-	}
-
-	public Set<OrderItem> getItems() {
-		return items;
+	public void setOrder(Order order) {
+		this.order = order;
 	}
 
 	@Override
@@ -116,7 +79,7 @@ public class Order implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Order other = (Order) obj;
+		Payment other = (Payment) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
