3d6e9e997aa4d6ba68e6815d4d9ac65ec1dfae9c
Danilson Sanches <dsanches06@outlook.com>
2020-03-28 05:48:23 +0000

    OrderStatus enum

--- src/main/java/com/project/springbootjpa/config/TestConfig.java
+++ src/main/java/com/project/springbootjpa/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.project.springbootjpa.entities.Order;
 import com.project.springbootjpa.entities.User;
+import com.project.springbootjpa.entities.enums.OrderStatus;
 import com.project.springbootjpa.repositories.OrderRepository;
 import com.project.springbootjpa.repositories.UserRepository;
 
@@ -32,10 +33,10 @@ public class TestConfig implements CommandLineRunner {
 		User u3 = new User(null, "Danilson Sanches", "dsanches@gmail", "123456789", "teste123");
 
 		// order
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-		Order o4 = new Order(null, Instant.parse("2020-03-12T15:21:22Z"), u3);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
+		Order o4 = new Order(null, Instant.parse("2020-03-12T15:21:22Z"), OrderStatus.DELIVERED, u3);
 
 		// salvar na tabela user
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
