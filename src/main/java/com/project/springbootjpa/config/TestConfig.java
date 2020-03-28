af4c358decb759ca565dfbbcad09ba725705e228
Danilson Sanches <dsanches06@outlook.com>
2020-03-28 07:27:58 +0000

    Many-to-many association with JoinTable

--- src/main/java/com/project/springbootjpa/config/TestConfig.java
+++ src/main/java/com/project/springbootjpa/config/TestConfig.java
@@ -41,17 +41,23 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "Danilson Sanches", "dsanches@gmail", "123456789", "teste123");
-
+		// salvar na tabela user
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		
 		// order
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 		Order o4 = new Order(null, Instant.parse("2020-03-12T15:21:22Z"), OrderStatus.DELIVERED, u3);
+		// salvar na tabela order
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
 
 		// category
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+		// salvar na tabela category
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
 		// products
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
@@ -59,17 +65,17 @@ public class TestConfig implements CommandLineRunner {
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-
-		// salvar na tabela user
-		userRepository.saveAll(Arrays.asList(u1, u2, u3));
-
-		// salvar na tabela order
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
-
-		// salvar na tabela category
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-
 		// salvar na tabela product
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		// adicionar categorias aos produtos
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		// salvar os produtos novamento para ver as associações com categorias
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 	}
