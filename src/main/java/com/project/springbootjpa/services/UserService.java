f47d465596cad3b9b345e8cbd88f918662536e43
Danilson Sanches <dsanches06@outlook.com>
2020-03-30 10:05:34 +0100

    Exception handling - update

--- src/main/java/com/project/springbootjpa/services/UserService.java
+++ src/main/java/com/project/springbootjpa/services/UserService.java
@@ -3,6 +3,8 @@ package com.project.springbootjpa.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -36,20 +38,22 @@ public class UserService {
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
-		} 
-		catch (EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		} 
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
-		// para obter a entidade, sem ter que ir a base de dados
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			// para obter a entidade, sem ter que ir a base de dados
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
